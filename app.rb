2d33573ffa65a86019e6090e6cfb3ab6262c32e3
aemrich <adam.emrich@gmail.com>
2016-12-14 19:11:44 -0500

    modded

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Welcome to your app!!!! I BUILT THIS. Well no I didn't really. BUT I WROTE THIS PART. AND USED SHOTGUN TO SEE IT!!"
   end
 
 end
